71b92db63c5fd37fc8804f73c65a6a34f8ce102c
Richard Dubois <dubois.richard@gmail.com>
2020-01-04 01:41:23 -0500

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
-FROM python:3
-#FROM python:3-onbuild
-
-RUN set -o pipefail && wget -O python.tar.xz "https://www.python.org/ftp/python/${PYTHON_VERSION%%[a-z]*}/Python-$PYTHON_VERSION.tar.xz"
+FROM python:3-onbuild
 
 COPY . /usr/src/app
 WORKDIR /usr/src/app
 ENV TZ=America/Montreal
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-CMD ["python", "test.py"]
 
-
+CMD ["python", "rtl-schedule.py"]
--- rtl-schedule.py
+++ rtl-schedule.py
@@ -1,26 +1,110 @@
 
 #import csv
 
+import zipfile
 
 import pandas
+import numpy
 
+# import wget
+import requests
 
+import os
+import os.path
+
+from flask import Flask, abort, Response, jsonify, request
+
+from flask_restful import Resource, Api
+
+import logging
+
+# create logger with 'rtl-schedule'
+logger = logging.getLogger('rtl-schedule')
+
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('rtl-schedule.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+app = Flask(__name__)
+api = Api(app)
 
 class ParseRTLData:
+
+    def __init__(self, schedule_zipfile='gtfs.zip'):
+
+        logger.error("os.getcwd():" + os.getcwd())
+
+        file = os.getcwd() + '/' + schedule_zipfile
+
+        if not (os.path.isfile(file)):
+            self.downloadGtfsFile(file)
+
+        self.schedule_zipfile = schedule_zipfile
+
+
+    def downloadGtfsFile(self, ZipFile_location):
+        input_url = "http://www.rtl-longueuil.qc.ca/transit/latestfeed/RTL.zip"
+        # wget.download(input_url, ZipFile)
+
+
+         
+        # url = 'https://readthedocs.org/projects/python-guide/downloads/pdf/latest/'
+         
+        myfile = requests.get(input_url, allow_redirects=True)
+         
+        open(ZipFile_location, 'wb').write(myfile.content)
+
 
 
 
     def getStopId(self, stop_code):
 
+        z = zipfile.ZipFile(self.schedule_zipfile)
 
-        stops = pandas.read_csv('stops.txt', index_col='stop_id')
-        stop = stops[stops.stop_code == stop_code]
+        # pandas.read_csv(z.open(z.infolist()[0].filename))
 
-        return stop.stop_id
+        file = z.open('stops.txt')
 
-        # print(df)
+        # stops = pandas.read_csv(file, index_col='stop_id')
+        stops = pandas.read_csv(file, index_col='stop_code')
 
-        # with open('stops.txt') as csv_file:
-        #     csv_reader = csv.reader(csv_file, delimiter=',')
+        # stop = stops.index(stop_code)
+        # stop = stops[stop_code == stop_code]
+        # stop = stops[stop_code == 32752]
+
+        # stop = numpy.where(stop_code == stop_code)
+        stop = numpy.where(stops == 32752)
+
+        logger.error(str(stops))
+        logger.error(str(stop))
+        logger.error(type(stop))
+        return "0"
+        # logger.error("Response: %s" % (stop.stop_id))
+        # return stop.stop_id
 
 
+class RtlScheduleNextStop(Resource):
+
+    def get(self, stop_code):
+        rtl_data = ParseRTLData(schedule_zipfile='gtfs.zip')
+        # return rtl_data.getStopId(stop_code)
+
+        return Response(rtl_data.getStopId(stop_code), mimetype='text/xml')
+
+
+
+api.add_resource(RtlScheduleNextStop, '/rtl_schedule/nextstop/<int:stop_code>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
