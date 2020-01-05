283ec193377558958a8d0e67e48c085079a1d3cb
Richard Dubois <dubois.richard@gmail.com>
2020-01-05 14:02:21 -0500

    Finalized the coding for RTL Schedule.

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
+# RTL-Schedule
 
+Return the time and info of the next bus for a giving bus-stop of the RTL.
 
-docker build . -t rtl-schedule
-docker run -p 80:80 rtl-schedule
+## Prerequisite
+
+- Stop Code number.
+
+# TODO List
+
+- [x] Finalize the coding.
+- [ ] Indicate how to find a stop code.
+- [ ] Create a Docker container registry.
+- [ ] Sometimes, the rage of date in the file `calendar.txt` in the current zip file is in the future.
+
+## Build
+
+`docker build . -t rtl-schedule`
+`docker run -p 80:80 rtl-schedule`
+
+## How to start it
+
+curl http://localhost/rtl_schedule/nextstop/32752
+
--- rtl-schedule.py
+++ rtl-schedule.py
@@ -1,19 +1,15 @@
-
-#import csv
-
 import zipfile
-
 import pandas
-# import numpy
 
-# import wget
 import requests
+
+import datetime
+import time
 
 import os
 import os.path
 
 from flask import Flask, abort, Response, jsonify, request
-
 from flask_restful import Resource, Api
 
 import logging
@@ -43,80 +39,196 @@ class ParseRTLData:
 
     def __init__(self, schedule_zipfile='gtfs.zip'):
 
-        logger.error("os.getcwd():" + os.getcwd())
-
         file = os.getcwd() + '/' + schedule_zipfile
 
         if not (os.path.isfile(file)):
             self.downloadGtfsFile(file)
+            logger.info("Downloaded a new zip file.")
 
         self.schedule_zipfile = schedule_zipfile
 
 
-    def downloadGtfsFile(self, ZipFile_location):
+    def downloadGtfsFile(self, ZipFile_location) -> None:
+
+        """ Download the GTFS file from the website, write it on disk. """
+
         input_url = "http://www.rtl-longueuil.qc.ca/transit/latestfeed/RTL.zip"
-        # wget.download(input_url, ZipFile)
-
-
-         
-        # url = 'https://readthedocs.org/projects/python-guide/downloads/pdf/latest/'
-         
         myfile = requests.get(input_url, allow_redirects=True)
          
         open(ZipFile_location, 'wb').write(myfile.content)
 
-        logger.info("Downloaded a new zip file.")
+    def getStopId(self, stop_code: int) -> int:
 
+        """ Retrieve the stop_id based on a stop_code """
 
+        with zipfile.ZipFile(self.schedule_zipfile) as myzip:
 
+            with myzip.open('stops.txt') as myfile:
 
-
-    def getStopId(self, stop_code):
-
-        z = zipfile.ZipFile(self.schedule_zipfile)
-
-        # pandas.read_csv(z.open(z.infolist()[0].filename))
-
-        file = z.open('stops.txt')
-
-        # stops = pandas.read_csv(file, index_col='stop_id')
-        stops = pandas.read_csv(file, index_col='stop_code')
-
-
-
-        row = stops.loc[stop_code, : ]
+                # pandas.read_csv(z.open(z.infolist()[0].filename))
+                stops = pandas.read_csv(myfile, index_col='stop_code')
 
         stopId_cellvalue = stops.loc[stop_code, : "stop_id" ].values[0]
 
+        return stopId_cellvalue
+
+    def getServiceId(self, date) -> int:
+
+        """ Retrive the service_id for a giving date """
+
+        # Return the day of the week as an integer, where Monday is 0 and Sunday is 6.
+        curr_weekday = date.weekday()
+
+        currDateInt = int(date.strftime("%Y%m%d"))
 
 
-        # stop = stops.index(stop_code)
-        # stop = stops[stop_code == stop_code]
-        # stop = stops[stop_code == 32752]
+        with zipfile.ZipFile(self.schedule_zipfile) as myzip:
+            with myzip.open('calendar.txt') as myfile:
 
-        # stop = numpy.where(stop_code == stop_code)
-        # stop = numpy.where(stops == 32752)
+                dataframe = pandas.read_csv(myfile)
 
-        # logger.error(str(stops))
-        # logger.error(str(row))
-        # logger.error(type(row))
-        logger.error(str(stopId_cellvalue))
-        logger.error(type(stopId_cellvalue))
+        for index, row in dataframe.iterrows():
 
-        return str(stopId_cellvalue)
-        # logger.error("Response: %s" % (stop.stop_id))
-        # return stop.stop_id
+            # Monday
+            # if ((curr_weekday == 0) and (row["monday"] == 1) and (row["start_date"] <= currDateInt) and (row["end_date"] >= currDateInt)):
+            if ((curr_weekday == 0) and (row["monday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Tuesday
+            if ((curr_weekday == 1) and (row["tuesday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Wednesday
+            if ((curr_weekday == 2) and (row["wednesday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Thursday
+            if ((curr_weekday == 3) and (row["thursday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Friday
+            if ((curr_weekday == 4) and (row["friday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Saturday
+            if ((curr_weekday == 5) and (row["saturday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+            # Sunday
+            if ((curr_weekday == 6) and (row["sunday"] == 1) and (row["end_date"] >= currDateInt)):
+                return row["service_id"]
+
+        # TODO: Unexpected, raise something
+        return -1
+
+
+    def getNextStop(self, stop_id: int, parm_datetime):
+
+        """Retrieve the next stop information"""
+
+        todayServiceId = self.getServiceId(parm_datetime.date())
+
+        stop_times = None
+        trips = None
+
+        # Join stop_times.txt avec trips.txt (key trip_id), trim stop_id. create a new dataframe
+        with zipfile.ZipFile(self.schedule_zipfile) as myzip:
+
+            with myzip.open('stop_times.txt') as myfile1:
+
+                stop_times = pandas.read_csv(myfile1, index_col='stop_id')
+
+            # Only keep rows where stop_id is our stop_id.
+            # Note using [[ ]] returns a DataFrame.
+            stop_times = stop_times.loc[[stop_id, ]]
+
+            with myzip.open('trips.txt') as myfile2:
+
+                trips = pandas.read_csv(myfile2)
+
+        results = stop_times.merge(trips, how='left', on='trip_id', validate='many_to_one')
+
+        # Add a date field. (today).
+        results['arrival_datetime'] = parm_datetime.date()
+
+        #Only today result subset
+        results = results.set_index('service_id')
+        todayResults = results.loc[[todayServiceId, ]]
+        todayResults = todayResults.reset_index()
+
+        # First loop is to patch the time field, then update datetime.
+        for index, row in todayResults.iterrows():
+
+            row_date = row["arrival_datetime"]
+            row_time_str = row["arrival_time"]
+
+            if (row["arrival_time"][:2] == '24'):
+
+                # We change the time as 00
+                row_time_str = ("00" + row["arrival_time"][2:])
+                todayResults.at[index , 'arrival_time'] = row_time_str
+
+                row_date = row_date + datetime.timedelta(days=1)
+
+            # Update the arrival datetime
+            time_h = int(row_time_str[:2])
+            time_m = int(row_time_str[3:5])
+            time_s = int(row_time_str[6:8])
+
+            row_time = datetime.time(hour=time_h, minute=time_m, second=time_s)
+            row_datetime = datetime.datetime.combine(row_date, row_time)
+
+            todayResults.at[index , 'arrival_datetime'] = row_datetime
+
+
+        todayResults['arrival_datetime'] = pandas.to_datetime(todayResults.arrival_datetime)
+
+        todayResults = todayResults.sort_values(by=['arrival_datetime'])
+
+        found_row = None
+
+        # Second loop is to find the next stop.
+        for index, row in todayResults.iterrows():
+
+            if (row['arrival_datetime'] <= parm_datetime):
+                continue
+            else:
+                found_row = row
+                break
+
+        # Row found
+        # logger.info(found_row)
+
+        return found_row
 
 
 class RtlScheduleNextStop(Resource):
 
+    """ Get the next stop informations in JSON format. """
+
     def get(self, stop_code):
         rtl_data = ParseRTLData(schedule_zipfile='gtfs.zip')
-        # return rtl_data.getStopId(stop_code)
 
-        return Response(rtl_data.getStopId(stop_code), mimetype='text/xml')
+        stop_id = rtl_data.getStopId(stop_code)
+
+        # Retrieve the next stop information from now
+        current_datetime = datetime.datetime.now().replace(microsecond=0)
+        nextStopRow = rtl_data.getNextStop(stop_id, current_datetime)
 
 
+        difference = nextStopRow.arrival_datetime - current_datetime
+        seconds_in_day = 24 * 60 * 60
+        nbr_minutes,nbr_seconds = divmod(difference.days * seconds_in_day + difference.seconds, 60)
+
+        result = { 'nextstop_nbrmins': nbr_minutes,
+                   'nextstop_nbrsecs': nbr_seconds,
+                   'route_id': nextStopRow.route_id,
+                   'arrival_time': nextStopRow.arrival_time,
+                   'trip_headsign': nextStopRow.trip_headsign,
+                   'current_time': str(current_datetime.time()),
+                 }
+
+        return jsonify(result)
 
 api.add_resource(RtlScheduleNextStop, '/rtl_schedule/nextstop/<int:stop_code>')
 
